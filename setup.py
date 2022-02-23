774a0417d6f161ace14846a9b0bed3d9785e86e8
Christian Werner <chr.werner@me.com>
2022-02-23 15:41:39 +0100

    fix distname

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 
 from setuptools import find_packages, setup
 
-DISTNAME = "stanic"
+DISTNAME = "stantic"
 LICENSE = "MIT"
 AUTHOR = "Christian Werner"
 AUTHOR_EMAIL = "christian.werner@kit.edu"
